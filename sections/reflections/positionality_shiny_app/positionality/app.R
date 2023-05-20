e5d7b124ab759ee65bdb67e657addffd2a3cb1cc
Brooke_Walters <MY_NAME@example.comBrooke.melissa.walters@gmail.com>
2023-05-20 14:52:09 -0400

    updated plots for ps graph

--- sections/reflections/positionality_shiny_app/positionality/app.R
+++ sections/reflections/positionality_shiny_app/positionality/app.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 
 ui <- fluidPage(
-    titlePanel("Interactive Timeline"),
+    titlePanel("Interactive Positionality Timeline"),
     mainPanel(
         plotlyOutput("timelinePlot")
     )
@@ -18,19 +18,20 @@ server <- function(input, output) {
         # the data
         timeline_data <- data.frame(
             id = 1:4,
-            positionality = c("Parental Socioeconomic Status", "Spirituality", "Nationality", "Race"),
+            positionality = c("Parental Socioeconomic Status", "Spirituality", "Nationality", "Racial Identity"),
             year = as.Date(c("1985-01-01", "2004-01-01", "2014-01-01", "2023-01-01")),
             description = c(
-                "'I don’t want to see you, and I don’t want to hear you.'
-                These were my father's nightly words for the summers devoid of mandated bedtimes. 
-                In those moments, I discovered a world of books, solitude, and one-thousand-piece jig-saw puzzles.
-                Being born to young parents instilled in me a spirit of self-reliance, yet also nurtured a hint of survivorship bias.
-                The days of food and housing insecurity now reside as nebulous memories, occasionally nudging me to dismiss the struggles faced by others.
-                I firmly believe in the importance of seeking out experiences that reconnect me with the shared humanity of others. 
-                Through art, literature, and the simple act of attentive listening, I regain empathy. 
-                Suffering is not a competition; the intensity of an individual's pain is their valid reality.",       
+                "'I don’t want to see you, I don’t want to hear you.' 
+                These were my father's nightly words during the summers devoid of enforced bedtimes. 
+                Summers were my portal into the realms of Steinbeck, Huxley, and L. Frank Baum. 
+                Being born to young parents, struggling to navigate their new adulthood, 
+                instilled within me a deep sense of self-reliance but also curated survivorship bias. 
+                The days of food and housing insecurity are now nebulous memories, 
+                occasionally nudging me to dismiss the struggles faced by others. 
+                Reconnecting with humanity through art, literature, community events, and coffee is the remedy to rekindle empathy. 
+                Suffering is not a competition; the intensity of an individual's pain is their valid reality.",
                 
-                "'You are an expression of the Universe.' 
+                "'Exsistance is an expression of the Universe.' ~ Alan Watts paraphrased. 
                 As a born-again evangelical Christian turned atheist, I perceive the world as it is,
                 deriving purpose and meaning from the ordinary intricacies of everyday existence. 
                 That is what I appreciate about science—the constant evolution of our understanding as new information emerges,
@@ -41,11 +42,12 @@ server <- function(input, output) {
                 I’ll never forget the moment the armored vehicle doors opened and I saw my first country outside of North America.
                 Rubble, garbage, unpaved roads, dirt. 
                 I spent my time in Afghanistan visiting local police stations and leading first aid classes. 
-                I recall one of my 'students' guiding my hand to the indentation on the back of his head. 
-                He bore the scars of an encounter with the Taliban's improvised explosive device (IED), the shrapnel having impaled him and leaving a bit of his skull missing.
-                Remarkably, he had packed his own wound and traversed over an hour to reach the sole hospital, where he underwent life-saving surgery provided by a NATO medical unit. 
+                On one such visit, a 'student' instructed me to palpate an unusual cranium hollow. 
+                It was the mark of an unfortunate encounter with an improvised explosive device (IED), and the Taliban's failure to end him. 
+                Remarkably, he packed his own head wound and traversed over an hour to reach the sole hospital, 
+                where he underwent life-saving surgery provided by a NATO medical unit. 
                 After this amazing story, I remarked that he should be leading the class, he joked that I should be his third wife. 
-                As an American, the probability of finding myself in a series of circumstances similar the police officer's is rather slim.",
+                As an American, the probability of finding myself in a series of circumstances similar to this police officer's is rather slim.",
                 
                 
                 "'Let me give you a tip. You wanna make some money here? Use your white voice.' 
@@ -58,15 +60,20 @@ server <- function(input, output) {
                 'Brooke, they gave you the wrong meal,' 
                 The mom tries to take my toy in exchange for the black Barbie doll that was meant for me. 
                 I refused and ruined the outing. 
-                I don’t blame a six-year-old for being disappointed, but that experience was one of many that ingrained in me the false notion that white was the ideal.
+                
+                I don’t blame a six-year-old for being disappointed,
+                but that experience was one of many that ingrained in me the false notion that white was the ideal.
                 It's more vivid than any of the racially explicit derogatory terms thrown at me by strangers. 
                 Trying to unlearn that mentality was a journey. 
                 Now that critical race theory has gone corporate, and I often find myself the only brown person in the room, 
                 I wonder just how honest I can be and keep my paycheck." 
             ),
-
+            
             pos_palette = c("#CBD1C9", "#F3938F", "#FCD470", "#CCAE8E")
         )
+        
+        timeline_data$positionality <- factor(timeline_data$positionality,
+                                              levels = c("Racial Identity", "Nationality","Spirituality", "Parental Socioeconomic Status"))
         
         # the plot
         pos_plot <- timeline_data %>%
@@ -79,14 +86,15 @@ server <- function(input, output) {
                 marker = list(color = ~pos_palette , size = 45)
             ) %>%
             layout(
-                title = "Hover To Read Positionality Statements",
+                title = "dimensions of my identity | hover over plots to read",
+                subtitle  = "Hover To Read",
                 xaxis = list(
                     title = "Year",
                     tickvals = as.Date(c("1985-01-01", "2004-01-01", "2014-01-01", "2023-01-01")),
                     ticktext = c("1985", "2004", "2014", "2023")
                 ),
-                yaxis = list(title = "positionality"),
-                hovermode = "closest",
+                yaxis = list(title = " "),
+                # hovermode = "closest",
                 hoverlabel = list(bordercolor = "black")
             )
         
